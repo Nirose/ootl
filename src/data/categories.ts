7158cc577f2bf888aebb69d6fe1354e0ad50af9b
Jorin Manandhar <nirose@jucktion.com>
2024-11-22 17:14:04 +0545

    add new categories to posts

--- src/data/categories.ts
+++ src/data/categories.ts
@@ -1,8 +1,9 @@
 // List of categories for blog posts
 export const CATEGORIES = [
-	'Category 1',
-	'Category 2',
-	'Category 3',
-	'Category 4',
-	'Category 5'
+	'Business',
+	'Technology',
+	'Entertainment',
+	'Sports',
+	'Science',
+	'Health'
 ] as const
