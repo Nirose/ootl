940033e52d572eb725eb59e59890f6938f4fd6b8
Jorin Manandhar <nirose@jucktion.com>
2025-11-12 18:30:48 +0545

    re-arrange the post editor page

--- tina/config.ts
+++ tina/config.ts
@@ -28,20 +28,23 @@ export default defineConfig({
 				format: 'mdx',
 				fields: [
 					{
-						type: 'image',
-						label: 'Cover Image',
-						required: true,
-						name: 'heroImage',
-						description: 'The image used for the cover of the post'
+						name: 'draft',
+						label: 'Draft',
+						type: 'boolean',
+						description: 'If this is checked the post will not be published'
 					},
-
+					{
+						type: 'datetime',
+						name: 'pubDate',
+						label: 'Publication Date',
+						required: true
+					},
 					{
 						type: 'string',
-						required: true,
-						name: 'category',
-						label: 'Category',
-						description: 'Select an category for this post',
-						options: [...CATEGORIES]
+						name: 'title',
+						label: 'Title',
+						isTitle: true,
+						required: true
 					},
 					{
 						type: 'string',
@@ -51,16 +54,19 @@ export default defineConfig({
 						description: 'A short description of the post'
 					},
 					{
-						type: 'datetime',
-						name: 'pubDate',
-						label: 'Publication Date',
-						required: true
+						type: 'image',
+						label: 'Cover Image',
+						required: true,
+						name: 'heroImage',
+						description: 'The image used for the cover of the post'
 					},
 					{
-						name: 'draft',
-						label: 'Draft',
-						type: 'boolean',
-						description: 'If this is checked the post will not be published'
+						type: 'string',
+						required: true,
+						name: 'category',
+						label: 'Category',
+						description: 'Select an category for this post',
+						options: [...CATEGORIES]
 					},
 					{
 						type: 'string',
@@ -73,13 +79,7 @@ export default defineConfig({
 							component: 'tags'
 						}
 					},
-					{
-						type: 'string',
-						name: 'title',
-						label: 'Title',
-						isTitle: true,
-						required: true
-					},
+
 					{
 						type: 'rich-text',
 						label: 'Body',
